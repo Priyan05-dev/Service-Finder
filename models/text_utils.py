773797788c39aa2b784a44264823a90189921abb
Priyan05-dev <santhipriyans05@gmail.com>
2026-07-25 15:23:42 +0530

    updated nltk

--- models/text_utils.py
+++ models/text_utils.py
@@ -1,22 +1,31 @@
 # Basic text cleaning using nltk, used before feeding text into sklearn models
 
 import nltk
+
+# Download required NLTK data if not already available
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+
+try:
+    nltk.data.find("corpora/wordnet")
+except LookupError:
+    nltk.download("wordnet")
+
+try:
+    nltk.data.find("corpora/omw-1.4")
+except LookupError:
+    nltk.download("omw-1.4")
+
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
-
-
-def clean_text(text):
-    text = text.lower()
-    tokens = word_tokenize(text)
-
-    cleaned = []
-    for word in tokens:
-        if word.isalpha() and word not in stop_words:
-            lemma = lemmatizer.lemmatize(word)
-            cleaned.append(lemma)
-
-    return " ".join(cleaned)
